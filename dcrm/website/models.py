1529a97a35553912fc55a525dd2d567909e7ae63
kapish <62199998+kapish97@users.noreply.github.com>
2023-08-20 15:03:11 +0530

    created model Record

--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name= models.CharField(max_length=25)
+    last_name  = models.CharField(max_length=25)
+    phone =models.CharField(max_length=10)
+    address= models.CharField(max_length=50)
+    city= models.CharField(max_length=15)
+    state = models.CharField(max_length=10)
+    pincode= models.CharField(max_length=6)
+
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name}")
